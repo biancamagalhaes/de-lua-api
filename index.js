9ce0619314a00c3cad8afb62e51c7b06f34f2a27
Bianca Magalhães <bianca.magalhaes2610@gmail.com>
2021-11-19 16:00:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 const port = 3000;
 
 const dotenv = require('dotenv');
 dotenv.config();
+
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+);
+app.options('*', cors());
+
+app.get('/', (req, res) => res.send('Estou Funcionando!'));
 
 app.use('/user', require('./route/user'));
 app.use('/psychologist', require('./route/psychologist'));
