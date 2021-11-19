e63a7995069ec3ccb3fb89684a57968fc9a1d86d
Bianca Magalhães <bianca.magalhaes2610@gmail.com>
2021-11-19 16:47:49 -0300

    Update postgresConnection.js

--- config/postgresConnection.js
+++ config/postgresConnection.js
@@ -1,4 +1,12 @@
 const pgp = require('pg-promise')();
-const db = pgp('postgres://6d2d9670fc15dd99d233a0bb007dae891dcd2962cd523faf36d76d8b297b3354@ec2-44-198-223-154.compute-1.amazonaws.com:5432/daa3sd4s3t6rnh?ssl=true');
+const cn = {
+    host: 'ec2-44-198-223-154.compute-1.amazonaws.com', // server name or IP address;
+    port: 5432,
+    database: 'daa3sd4s3t6rnh',
+    user: 'kwnfprkjxqpmmi',
+    password: '6d2d9670fc15dd99d233a0bb007dae891dcd2962cd523faf36d76d8b297b3354',
+    ssl: { rejectUnauthorized: false }
+};
+const db = pgp(cn);
 
 module.exports = db;
